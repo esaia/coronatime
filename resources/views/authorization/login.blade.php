a9a9ffa685ec94850d7ade44ab897267bf5f9de8
esaia <esaiagafrindashvili@gmail.com>
2023-04-12 11:23:53 +0400

    initial project

---
+++ resources/views/authorization/login.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <auth-layout>
+        <h2>login</h2>
+    </auth-layout>
+</x-layout>
---
+++ resources/views/components/auth-layout.blade.php
@@ -0,0 +1,18 @@
+<div class="flex">
+
+
+    {{-- Form --}}
+
+
+    <div class="flex-3">
+    </div>
+
+    {{-- Image --}}
+
+
+
+    <div class="flex-1">
+    </div>
+
+
+</div>
