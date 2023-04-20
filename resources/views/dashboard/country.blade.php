3eff810b0b95c9dec6387d700980068f998b4a2c
esaia <esaiagafrindashvili@gmail.com>
2023-04-20 10:23:07 +0400

    refactor: create data table with grid mode

--- resources/views/dashboard/country.blade.php
+++ resources/views/dashboard/country.blade.php
@@ -10,109 +10,98 @@
             placeholder="{{ __('dashboard.search_placeholder') }}" value="{{ request('search') }}">
     </form>
 
-    <div class="relative overflow-x-auto shadow-md rounded-md  max-h-[500px] ">
-        <table class="w-full text-sm text-left text-gray-800   ">
 
-            <thead class="text-xs text-black  bg-gray-50  w-full sticky top-0  ">
-                <tr class="w-full ">
-                    <th scope="col" class="px-6 py-3 cursor-pointer ">
-                        <div class="flex items-center">
-                            <a
-                                href="/country?sortBy=name&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
-                                {{ __('dashboard.location') }}
 
-                            </a>
+    <div
+        class="grid grid-cols-1 w-full    rounded-md shadow-md  border border-gray-50 overflow-auto  overflow-y-hidden ">
 
-                            @if (request('sortBy') == 'name')
-                                @if (request('sortOrder') == 'desc')
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
-                                @else
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
-                                @endif
-                            @else
-                                <img src="images/sort_none.svg" alt="" class="w-6 h-6">
-                            @endif
-                        </div>
-                    </th>
-                    <th scope="col" class="px-6 py-3 cursor-pointer ">
-                        <div class="flex items-center">
-                            <a
-                                href="/country?sortBy=confirmed&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
-                                {{ __('dashboard.new_cases') }}
-                            </a>
-                            @if (request('sortBy') == 'confirmed')
-                                @if (request('sortOrder') == 'desc')
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
-                                @else
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
-                                @endif
-                            @else
-                                <img src="images/sort_none.svg" alt="" class="w-6 h-6">
-                            @endif
-                        </div>
+        <div class="w-full flex  bg-gray-50 rounded-t-md  min-w-[800px]   ">
+            <div scope="row" class="thead">
+                <a
+                    href="/country?sortBy=name&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
+                    {{ __('dashboard.location') }}
 
-                    </th>
-                    <th scope="col" class="px-6 py-3 cursor-pointer ">
-                        <div class="flex items-center">
+                </a>
+                @if (request('sortBy') == 'name')
+                    @if (request('sortOrder') == 'desc')
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
+                    @else
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
+                    @endif
+                @else
+                    <img src="images/sort_none.svg" alt="" class="w-6 h-6">
+                @endif
+            </div>
+            <div class="thead ">
+                <a
+                    href="/country?sortBy=confirmed&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
+                    {{ __('dashboard.new_cases') }}
+                </a>
+                @if (request('sortBy') == 'confirmed')
+                    @if (request('sortOrder') == 'desc')
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
+                    @else
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
+                    @endif
+                @else
+                    <img src="images/sort_none.svg" alt="" class="w-6 h-6">
+                @endif
+            </div>
+            <div class="thead ">
+                <a
+                    href="/country?sortBy=deaths&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
+                    {{ __('dashboard.death') }}
+                </a>
+                @if (request('sortBy') == 'deaths')
+                    @if (request('sortOrder') == 'desc')
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
+                    @else
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
+                    @endif
+                @else
+                    <img src="images/sort_none.svg" alt="" class="w-6 h-6">
+                @endif
+            </div>
+            <div class="thead ">
+                <a
+                    href="/country?sortBy=recovered&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
+                    {{ __('dashboard.recovered') }}
 
-                            <a
-                                href="/country?sortBy=deaths&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
-                                {{ __('dashboard.death') }}
-                            </a>
-                            @if (request('sortBy') == 'deaths')
-                                @if (request('sortOrder') == 'desc')
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
-                                @else
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
-                                @endif
-                            @else
-                                <img src="images/sort_none.svg" alt="" class="w-6 h-6">
-                            @endif
-                        </div>
-                    </th>
-                    <th scope="col" class="px-6 py-3 cursor-pointer ">
-                        <div class="flex items-center">
-                            <a
-                                href="/country?sortBy=recovered&sortOrder={{ request('sortOrder') == 'asc' ? 'desc' : 'asc' }}&{{ http_build_query(request()->except('sortBy', 'sortOrder')) }}">
-                                {{ __('dashboard.recovered') }}
+                </a>
 
-                            </a>
+                @if (request('sortBy') == 'recovered')
+                    @if (request('sortOrder') == 'desc')
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
+                    @else
+                        <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
+                    @endif
+                @else
+                    <img src="images/sort_none.svg" alt="" class="w-6 h-6">
+                @endif
+            </div>
+        </div>
 
-                            @if (request('sortBy') == 'recovered')
-                                @if (request('sortOrder') == 'desc')
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6">
-                                @else
-                                    <img src="images/sort_desc.svg" alt="" class="w-6 h-6 rotate-180">
-                                @endif
-                            @else
-                                <img src="images/sort_none.svg" alt="" class="w-6 h-6">
-                            @endif
-                        </div>
+        <div class=" max-h-[500px] min-w-[800px] overflow-y-auto  ">
 
-                    </th>
-                </tr>
-            </thead>
+            @foreach ($countries as $country)
+                <div class="w-full flex border-b ">
 
-            <tbody>
-                @foreach ($countries as $country)
-                    <tr class="bg-white border-b ">
-                        <th scope="row" class="px-6 py-4 font-medium ">
-                            {{ $country['name'] }}
-                        </th>
-                        <td class="px-6 py-4">
-                            {{ $country['confirmed'] }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $country['deaths'] }}
-                        </td>
-                        <td class="px-6 py-4">
-                            {{ $country['recovered'] }}
-                        </td>
-                    </tr>
-                @endforeach
-            </tbody>
+                    <div class="px-6 py-4 flex-1   ">
+                        {{ $country['name'] }}
+                    </div>
+                    <div class="px-6 py-4 flex-1    ">
+                        {{ $country['confirmed'] }}
+                    </div>
+                    <div class="px-6 py-4 flex-1 ">
+                        {{ $country['deaths'] }}
+                    </div>
+                    <div class="px-6 py-4 flex-1   ">
+                        {{ $country['recovered'] }}
 
-        </table>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 
 
