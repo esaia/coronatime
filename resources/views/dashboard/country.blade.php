65b768f46f6c695febfdc3692f7ec5f224b5be49
esaia <esaiagafrindashvili@gmail.com>
2023-04-26 12:42:21 +0400

    style: fix padding and margin problem on statistic table

--- resources/views/dashboard/country.blade.php
+++ resources/views/dashboard/country.blade.php
@@ -3,7 +3,7 @@
 <x-dashboard.layout>
 
     <form method="GET" action=""
-        class="my-8  border border-transparent md:border-gray-100 w-full md:max-w-xs flex justify-start items-center rounded-md  ">
+        class="my-8  border border-transparent md:border-gray-100 w-full md:max-w-xs flex justify-start items-center rounded-md mx-5">
         <div class="pl-4">
             <x-icons.search />
         </div>
@@ -13,150 +13,153 @@
     </form>
 
 
-    <div
-        class="grid grid-cols-1 w-full    rounded-md shadow-md  border border-gray-50  overflow-x-hidden overflow-y-hidden ">
+    <div class="md:mx-5">
+        <div
+            class="grid grid-cols-1 w-full  rounded-md shadow-md  border border-gray-50  overflow-x-hidden overflow-y-hidden ">
 
-        <div class="w-full flex  bg-gray-50 rounded-t-md     ">
-            <div scope="row" class="thead  min-w-[20px]">
-                <a
-                    href="{{ route(
-                        'dashboard.country',
-                        array_merge(
-                            [
-                                'sortBy' => 'name',
-                                'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
-                            ],
-                            request()->except('sortBy', 'sortOrder'),
-                        ),
-                    ) }}">
-                    {{ __('dashboard.location') }}
+            <div class="w-full flex  bg-gray-50 rounded-t-md     ">
+                <div scope="row" class="thead  min-w-[20px]">
+                    <a
+                        href="{{ route(
+                            'dashboard.country',
+                            array_merge(
+                                [
+                                    'sortBy' => 'name',
+                                    'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
+                                ],
+                                request()->except('sortBy', 'sortOrder'),
+                            ),
+                        ) }}">
+                        {{ __('dashboard.location') }}
 
-                </a>
-                <div class="w-20">
-                    @if (request('sortBy') == 'name')
-                        @if (request('sortOrder') == 'desc')
-                            <x-icons.sort-desc />
+                    </a>
+                    <div class="w-20">
+                        @if (request('sortBy') == 'name')
+                            @if (request('sortOrder') == 'desc')
+                                <x-icons.sort-desc />
+                            @else
+                                <x-icons.sort-asc />
+                            @endif
                         @else
-                            <x-icons.sort-asc />
+                            <x-icons.sort-none />
                         @endif
-                    @else
-                        <x-icons.sort-none />
-                    @endif
+                    </div>
+
                 </div>
-
-            </div>
-            <div class="thead ">
-                <a
-                    href="{{ route(
-                        'dashboard.country',
-                        array_merge(
-                            [
-                                'sortBy' => 'confirmed',
-                                'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
-                            ],
-                            request()->except('sortBy', 'sortOrder'),
-                        ),
-                    ) }}">
-                    {{ __('dashboard.new_cases') }}
-                </a>
-                <div class="w-20">
+                <div class="thead ">
+                    <a
+                        href="{{ route(
+                            'dashboard.country',
+                            array_merge(
+                                [
+                                    'sortBy' => 'confirmed',
+                                    'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
+                                ],
+                                request()->except('sortBy', 'sortOrder'),
+                            ),
+                        ) }}">
+                        {{ __('dashboard.new_cases') }}
+                    </a>
+                    <div class="w-20">
 
 
-                    @if (request('sortBy') == 'confirmed')
-                        @if (request('sortOrder') == 'desc')
-                            <x-icons.sort-desc />
+                        @if (request('sortBy') == 'confirmed')
+                            @if (request('sortOrder') == 'desc')
+                                <x-icons.sort-desc />
+                            @else
+                                <x-icons.sort-asc />
+                            @endif
                         @else
-                            <x-icons.sort-asc />
+                            <x-icons.sort-none />
                         @endif
-                    @else
-                        <x-icons.sort-none />
-                    @endif
+                    </div>
+
                 </div>
+                <div class="thead ">
+                    <a
+                        href="{{ route(
+                            'dashboard.country',
+                            array_merge(
+                                [
+                                    'sortBy' => 'deaths',
+                                    'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
+                                ],
+                                request()->except('sortBy', 'sortOrder'),
+                            ),
+                        ) }}">
+                        {{ __('dashboard.death') }}
+                    </a>
+                    <div class="w-20">
 
-            </div>
-            <div class="thead ">
-                <a
-                    href="{{ route(
-                        'dashboard.country',
-                        array_merge(
-                            [
-                                'sortBy' => 'deaths',
-                                'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
-                            ],
-                            request()->except('sortBy', 'sortOrder'),
-                        ),
-                    ) }}">
-                    {{ __('dashboard.death') }}
-                </a>
-                <div class="w-20">
-
-                    @if (request('sortBy') == 'deaths')
-                        @if (request('sortOrder') == 'desc')
-                            <x-icons.sort-desc />
+                        @if (request('sortBy') == 'deaths')
+                            @if (request('sortOrder') == 'desc')
+                                <x-icons.sort-desc />
+                            @else
+                                <x-icons.sort-asc />
+                            @endif
                         @else
-                            <x-icons.sort-asc />
+                            <x-icons.sort-none />
                         @endif
-                    @else
-                        <x-icons.sort-none />
-                    @endif
+                    </div>
+
                 </div>
+                <div class="thead ">
+                    <a
+                        href="{{ route(
+                            'dashboard.country',
+                            array_merge(
+                                [
+                                    'sortBy' => 'recovered',
+                                    'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
+                                ],
+                                request()->except('sortBy', 'sortOrder'),
+                            ),
+                        ) }}">
+                        {{ __('dashboard.recovered') }}
 
-            </div>
-            <div class="thead ">
-                <a
-                    href="{{ route(
-                        'dashboard.country',
-                        array_merge(
-                            [
-                                'sortBy' => 'recovered',
-                                'sortOrder' => request('sortOrder') == 'asc' ? 'desc' : 'asc',
-                            ],
-                            request()->except('sortBy', 'sortOrder'),
-                        ),
-                    ) }}">
-                    {{ __('dashboard.recovered') }}
+                    </a>
 
-                </a>
+                    <div class="w-20">
 
-                <div class="w-20">
-
-                    @if (request('sortBy') == 'recovered')
-                        @if (request('sortOrder') == 'desc')
-                            <x-icons.sort-desc />
+                        @if (request('sortBy') == 'recovered')
+                            @if (request('sortOrder') == 'desc')
+                                <x-icons.sort-desc />
+                            @else
+                                <x-icons.sort-asc />
+                            @endif
                         @else
-                            <x-icons.sort-asc />
+                            <x-icons.sort-none />
+
                         @endif
-                    @else
-                        <x-icons.sort-none />
+                    </div>
 
-                    @endif
                 </div>
-
             </div>
-        </div>
 
-        <div class=" max-h-[500px]  overflow-x-hidden  overflow-y-auto  ">
+            <div class=" max-h-[500px]  overflow-x-hidden  overflow-y-auto  ">
 
-            @foreach ($countries as $country)
-                <div class="w-full flex border-b ">
+                @foreach ($countries as $country)
+                    <div class="w-full flex border-b ">
 
-                    <div class="p-4 flex-1 flex min-w-[20px]  ">
-                        {{ $country['name'] }}
-                    </div>
-                    <div class="p-4 flex-1 flex   ">
-                        {{ $country['confirmed'] }}
-                    </div>
-                    <div class="p-4 flex-1 flex">
-                        {{ $country['deaths'] }}
-                    </div>
-                    <div class="p-4 flex-1 flex  ">
-                        {{ $country['recovered'] }}
+                        <div class="p-4 flex-1 flex min-w-[20px]  ">
+                            {{ $country['name'] }}
+                        </div>
+                        <div class="p-4 flex-1 flex   ">
+                            {{ $country['confirmed'] }}
+                        </div>
+                        <div class="p-4 flex-1 flex">
+                            {{ $country['deaths'] }}
+                        </div>
+                        <div class="p-4 flex-1 flex  ">
+                            {{ $country['recovered'] }}
 
+                        </div>
                     </div>
-                </div>
-            @endforeach
+                @endforeach
+            </div>
         </div>
     </div>
+
 
 
 </x-dashboard.layout>
